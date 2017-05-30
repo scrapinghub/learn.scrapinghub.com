95196f43e34bae3339bb39a4c83c02db64594e39
Valdir Stumm Junior <stummjr@gmail.com>
2017-05-30 17:50:45 -0300

    add a section about JS pages on learn scrapy

--- _includes/spiders/7.py
+++ _includes/spiders/7.py
@@ -1,4 +1,33 @@
-$ pip install shub
-$ shub login
-$ shub deploy
-$ shub schedule quotes
+import scrapy
+from scrapy_splash import SplashRequest
+
+
+class QuotesJSSpider(scrapy.Spider):
+    name = 'quotesjs'
+    # all these settings can be put in your project's settings.py file
+    custom_settings = {
+        'SPLASH_URL': 'http://localhost:8050',
+        'DOWNLOADER_MIDDLEWARES': {
+            'scrapy_splash.SplashCookiesMiddleware': 723,
+            'scrapy_splash.SplashMiddleware': 725,
+            'scrapy.downloadermiddlewares.httpcompression.HttpCompressionMiddleware': 810,
+        },
+        'SPIDER_MIDDLEWARES': {
+            'scrapy_splash.SplashDeduplicateArgsMiddleware': 100,
+        },
+        'DUPEFILTER_CLASS': 'scrapy_splash.SplashAwareDupeFilter',
+    }
+
+    def start_requests(self):
+        yield SplashRequest(
+            url='http://quotes.toscrape.com/js',
+            callback=self.parse,
+        )
+
+    def parse(self, response):
+        for quote in response.css("div.quote"):
+            yield {
+                'text': quote.css("span.text::text").extract_first(),
+                'author': quote.css("small.author::text").extract_first(),
+                'tags': quote.css("div.tags > a.tag::text").extract(),
+            }
